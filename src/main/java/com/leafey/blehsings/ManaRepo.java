6e3584028005d3748adb08f960567d0a30472cb5
LeafeyTMM <tesok.milan@gmail.com>
2024-11-29 21:27:23 +0100

    Explanation for dumbasses

---
+++ src/main/java/com/leafey/blehsings/ManaRepo.java
@@ -0,0 +1,5 @@
+package com.leafey.blehsings;
+
+public class ManaRepo {
+    //FIXME Make Dictionary of player's nbt and max mana. Current mana should not be saved outside NBT, that will cause a lot of processing every time a play casts a spell or regenerates some mana!
+}
